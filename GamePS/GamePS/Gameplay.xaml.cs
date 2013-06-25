99d686d1d4dbfa36050652ac08eb4808b554fa34
lassi <lassi.laati@lut.fi>
2013-06-25 14:53:31 +0300

    Broke it even more

--- GamePS/GamePS/Gameplay.xaml.cs
+++ GamePS/GamePS/Gameplay.xaml.cs
@@ -24,32 +24,34 @@ namespace GamePS
     /// </summary>
     public sealed partial class Stage2 : Page
     {
-        int player_x, player_y;
-        public int scores = 3;
+        int player_x, player_y; //players coordinates, for debugging
+        public int scores = 0;  
         public bool Hitmonkey = false;
-        DateTime _dtLastFpsUpdate;
+        DateTime _dtLastFpsUpdate;  //text update frequen
         DateTime MankeeTime;  //monkeys movement time
         DateTime spawntime; // spawn time for rocks
-        PhysicsSprite monkey;
-        PhysicsSprite player;
+        PhysicsSprite monkey; //monkeys
+        PhysicsSprite player; // now apple, but later on the bat
         public Stage2()
         {
             this.InitializeComponent();
-            cnvGame1.Collision += new PhysicsCanvas.CollisionHandler(cnvGame_Collision);
-            cnvGame1.TimerLoop += new PhysicsCanvas.TimerLoopHandler(cnvGame_TimerLoop);
-            //CreateObs();
+            cnvGame1.TransformRoot = new CompositeTransform();
+            cnvGame1.Collision += new PhysicsCanvas.CollisionHandler(cnvGame_Collision); //update collisions
+            cnvGame1.TimerLoop += new PhysicsCanvas.TimerLoopHandler(cnvGame_TimerLoop); //update time
+            CreateObs();
             
         }
 
         public void init()
         {
-            PhysicsSprite monkey_temp = cnvGame1.PhysicsObjects["mankee"];
+            PhysicsSprite monkey_temp = cnvGame1.PhysicsObjects["mankee"]; //Creating a temp sprite
             PhysicsSprite player_temp = cnvGame1.PhysicsObjects["ball1"];
             monkey = monkey_temp;
             player = player_temp;
             
         }
 
+        //Time based actions
         void cnvGame_TimerLoop(object source)
         {
             player_y = (int)ball1.Position.Y;
@@ -64,7 +66,7 @@ namespace GamePS
                 
                 MankeeTime = DateTime.Now;
             }
-            //Display text time
+            //Display text 
             if ((DateTime.Now - _dtLastFpsUpdate).TotalMilliseconds > 50)
             {
                 scorecount.Text = String.Format("Score: {0} {1},{2}", scores, player_x, player_y);
@@ -72,22 +74,26 @@ namespace GamePS
             }
 
             //Spawn the rocks
-            if ((DateTime.Now - spawntime).TotalMilliseconds > 3500)
+            if ((DateTime.Now - spawntime).TotalMilliseconds > 4500)
             {
                 spawn();
                 spawntime = DateTime.Now;
             }
+
+            //Camera, to follow the player
+            
+            cnvGame1.TransformRoot.TranslateX = -ball1.Position.X;
+            cnvGame1.TransformRoot.TranslateY = -ball1.Position.Y;
             
         }
 
+        // SCORE!!!
         public void PlayerScores()
         {
-            
-            //this.Frame.Navigate(typeof(GameOver));
             scores++;
         }
 
-        //To check if player hit mankee, any collectables, goal
+        //To check if player hit mankee, any collectables, goal, etc...
         void cnvGame_Collision(PhysicsSprite sprite1, PhysicsSprite sprite2, FarseerPhysics.Dynamics.Contacts.Contact contact)
         {
             // this event is fired for each sprite to sprite collision
@@ -109,14 +115,14 @@ namespace GamePS
                 cnvGame1.DeletePhysicsObject(sprite2.Name);
             }
             
-            //if hit the floor
+            //if ball hits the floor
             if (sprite1.Name.StartsWith("rocks") && sprite2.Name == "ground")
             {
                 cnvGame1.DeletePhysicsObject(sprite1.Name);
                 scores--;
             }
 
-            //if goes off the screen, scores
+            //if goes off the screen from the sides, scores!
             if (sprite1.Name.StartsWith("rocks") && sprite2.Name.StartsWith("destroy"))
             {
                 cnvGame1.DeletePhysicsObject(sprite1.Name);
@@ -138,11 +144,11 @@ namespace GamePS
 
         Random random = new Random();
 
-        //Creates balls to the canvas
+        //Creates balls to the canvas, drops from above
         public void spawn()
         {
             int x = random.Next(0, 1300);
-            int y = random.Next(-100, -10);
+            int y = random.Next(-300, -250);
             Box bx = new Box();
             cnvGame1.AddPhysicsUserControl(bx, x, y, 1);
             
@@ -168,20 +174,19 @@ namespace GamePS
                 length = dir.Length();
                 // let's make a unit vector out of it
                 dir.Normalize();
-                //dir *= 0.7f;
-                //Apply force to the balls in the direction that userd touched on the screen
+                //Apply force to the balls in the direction that user touched on the screen
                 if (spr.Name == "ball1" || spr.Name == "ball2")
                     spr.BodyObject.ApplyLinearImpulse(dir);
+                    
 
 
             }
         }
 
-        //Moves the monkey == mankee in random direction
+        //Moves the monkey == mankee in the direction of the player (apple for now)
         public void mankee_move()
         {
             
-            //direction.Normalize();
             List<PhysicsSprite> physicsList = cnvGame1.PhysicsObjects.Values.ToList();
             for (int i = 0; i < physicsList.Count; i++)
             {
@@ -190,19 +195,20 @@ namespace GamePS
                 {
                     Vector2 pos;
                     pos.X = player_x;
-                    pos.Y = player_y;
-                    Vector2 direction = Vector2.Normalize(pos - spr.Position);
+                    pos.Y = player_y - 20;
+                    Vector2 direction = Vector2.Normalize(pos - spr.Position); //Direction = end point - start point
                     spr.BodyObject.ApplyLinearImpulse(direction);
                 }
             }
         }
 
         // RoutedEventArgs e
+        // When user taps the screen, move the ball in the touched direction
         public void uTouched(object sender, TappedRoutedEventArgs e)
         {
-            //pt is the position where th euser touched on the canvas
+            //pt is the position where the user touched on the canvas
             Point pt = e.GetPosition(cnvGame1);
-            // create a vector2 of the point, to move the objects in the pressed location
+            // create a vector2 of the point, to move the objects in the pressed location, move() function needs it info in Vec2
             Vector2 direction;
             direction.X = (float)pt.X;
             direction.Y = (float)pt.Y;
@@ -212,6 +218,7 @@ namespace GamePS
         }
 
         //Deletes all physics objects except walls/ground / obstacles
+        //Not used now
         public void DeleteAll(object sender, RoutedEventArgs e)
         {
             List<PhysicsSprite> lista = cnvGame1.PhysicsObjects.Values.ToList();
@@ -228,6 +235,7 @@ namespace GamePS
         }
 
         //Creates obstacles in randomized locations
+        //Not in use at the moment
         public void CreateObs()
         {
             int x, y;
@@ -235,30 +243,17 @@ namespace GamePS
             {
                 Obstacle obs = new Obstacle();
 
-                x = random.Next(50, (int)(cnvGame1.ActualWidth - (obs.ActualWidth + right_wall.ActualWidth)));
-                y = random.Next(50, (int)(cnvGame1.ActualHeight - (obs.ActualHeight + ground.ActualHeight)));
+                x = random.Next(50, 1000);
+                y = random.Next(50, 1000);
                 cnvGame1.AddPhysicsUserControl(obs, x, y);
             }
         }
 
+        //Function for go home button
         public void go_back(object sender, RoutedEventArgs e)
         {
             this.Frame.GoBack();
         }
-
-        public void AAA(object sender, RoutedEventArgs e)
-        {
-            //set_collision_groups();
-            init();
-            CreateObs();
-        }
-
-        //public void set_collision_groups()
-        //{
-        //    PhysicsSprite spr = cnvGame1.PhysicsObjects["ball1"];
-        //    spr.BodyObject.CollisionCategories = FarseerPhysics.Dynamics.Category.Cat2;
-        //    spr.BodyObject.CollidesWith = FarseerPhysics.Dynamics.Category.Cat2;
-        //}
 
     }
 
